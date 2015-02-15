7b27681cb10d81f2f664f74af3e8d2f9249ee82f
BowenChan <BowenmChan@gmail.com>
2015-02-14 18:44:08 -0800

    Working on the skeleton
    Working on the basic looking and creating the button.php action

---
+++ index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<link rel = "stylesheet" type= "text/css" href = "style.css"/>
+<tittle> </tittle>
+</head>
+<body>
+	<form action = "button.php">
+
+		<input type = "text" placeholder = "Event" name = "event"></input>
+		<button type = "button" value = "Create"> Create New </button>
+		<button type = "button" value = "Load"> Load </button>
+
+	</form>
+	
+	<table>
+		<tr>
+			<td> Task </td>
+			<td> Description </td>
+			<td> Duration </td>
+			<td> Predecessor </td>
+			<td> Resource </td>
+			<td> Assigned To </td>
+		</tr>
+
+	</table>
+</body>
+</html>
+
+
